3f29c4b93e44128db73cabd6f5d8304d36dfcaec
rhaendel <rhaendel@users.noreply.github.com>
2024-12-29 17:03:48 +0100

    Solution Day 03, part one

---
+++ src/Day03.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val regex = """mul\((\d+),(\d+)\)""".toRegex()
+
+    fun findAndMultiply(line: String): Int {
+        return regex.findAll(line)
+            .map { match ->
+                match.groups[1]!!.value.toInt() * match.groups[2]!!.value.toInt()
+            }
+            .sum()
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.map(::findAndMultiply).sum()
+    }
+
+    // Test if implementation meets criteria from the description, like:
+    check(part1(listOf("blamul(2,3)*x_mul(2,2)+mul(32,64]t")) == 10)
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 161)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day03")
+    part1(input).println()
+}
