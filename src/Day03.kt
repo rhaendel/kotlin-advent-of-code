ac9b7ee3f5209d3d21aa4754a828a00beddd07fb
rhaendel <rhaendel@users.noreply.github.com>
2024-12-29 18:18:53 +0100

    Solution Day 03, part two

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,35 @@
 fun main() {
-    val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+    val mulRegex = """(?<mul>mul\((\d{1,3}),(\d{1,3})\))"""
+    val doRegex = """(?<do>do\(\))"""
+    val dontRegex = """(?<dont>don't\(\))"""
 
-    fun findAndMultiply(line: String) = regex
+    fun multiply(match: MatchResult): Int {
+        val (_, left, right) = match.destructured
+        return left.toInt() * right.toInt()
+    }
+
+    fun findAndMultiply(line: String) = mulRegex
+        .toRegex()
         .findAll(line)
-        .sumOf {
-            val (left, right) = it.destructured
-            left.toInt() * right.toInt()
-        }
+        .sumOf(::multiply)
 
     fun part1(input: List<String>): Int {
         return input.map(::findAndMultiply).sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        var doIt = true
+
+        return input
+            .flatMap { line ->
+                "$mulRegex|$doRegex|$dontRegex"
+                    .toRegex()
+                    .findAll(line)
+            }.sumOf { match ->
+                match.groups["do"]?.let { doIt = true }
+                match.groups["dont"]?.let { doIt = false }
+                match.groups["mul"]?.let { if (doIt) multiply(match) else 0 } ?: 0
+            }
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -19,7 +39,11 @@ fun main() {
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 161)
 
+    check(part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 48)
+    check(part2(testInput) == 161)
+
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
     part1(input).println()
+    part2(input).println()
 }
