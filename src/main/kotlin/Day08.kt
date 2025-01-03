81ba194ce80c7c2053c9accb24a22e601871a3af
rhaendel <rhaendel@users.noreply.github.com>
2025-01-03 17:03:29 +0100

    Solution Day 08, part two

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,16 +1,12 @@
 import de.ronny_h.extensions.Coordinates
 import de.ronny_h.extensions.combinations
-import de.ronny_h.extensions.times
 
 fun main() {
     val day = "Day08"
 
     println("$day part 1")
 
-    fun part1(input: List<String>): Int {
-        val map = CityMap(input)
-        return map.collectAntinodes()
-    }
+    fun part1(input: List<String>) = CityMap(input).collectAntinodes().size
 
     printAndCheck(
         """
@@ -28,6 +24,31 @@ fun main() {
 
     val input = readInput(day)
     printAndCheck(input, ::part1, 214)
+
+
+    println("$day part 2")
+
+    fun part2(input: List<String>): Int {
+        val map = CityMap(input)
+        // map.printGrid()
+        val antinodes = map.collectAntinodes(withResonantHarmonics = true)
+        // map.printGrid(antinodes)
+        return antinodes.size
+    }
+
+    printAndCheck(
+        """
+            A...b
+            ....b
+            ..A..
+            ..cc.
+            .....
+        """.trimIndent().lines(),
+        ::part2, 11
+    )
+
+    printAndCheck(testInput, ::part2, 34)
+    printAndCheck(input, ::part2, 809)
 }
 
 private class CityMap(input: List<String>) {
@@ -37,9 +58,13 @@ private class CityMap(input: List<String>) {
     private val colIndices = input[0].indices
     private val rowIndices = input.indices
 
+    // just for printing; the algorithm itself does not need the complete grid
+    private val grid = Array(input.size) { CharArray(input[0].length) }
+
     private val antennas: Map<Char, List<Coordinates>> = HashMap<Char, MutableList<Coordinates>>().apply {
         input.forEachIndexed { row, line ->
             line.forEachIndexed { col, char ->
+                grid[row][col] = char
                 if (char != empty) {
                     getOrPut(char, ::mutableListOf).add(Coordinates(row, col))
                 }
@@ -47,22 +72,45 @@ private class CityMap(input: List<String>) {
         }
     }
 
-    fun collectAntinodes(): Int {
+    fun collectAntinodes(withResonantHarmonics: Boolean = false): HashSet<Coordinates> {
         val antinodes = HashSet<Coordinates>()
 
         antennas.entries.forEach { entry ->
             val antennasOfSameFrequency = entry.value
+            if (withResonantHarmonics) {
+                antinodes.addAll(antennasOfSameFrequency)
+            }
             antennasOfSameFrequency.combinations().forEach {
-                // add the difference vector to the second: second + (second - first)
-                val antinode = 2 * it.second - it.first
-                if (antinode.isOnTheMap()) {
-                    antinodes.add(antinode)
+                val diff = it.second - it.first
+                var antinode = it.second + diff
+                if (withResonantHarmonics) {
+                    while (antinode.isOnTheMap()) {
+                        antinodes.add(antinode)
+                        antinode += diff
+                    }
+                } else {
+                    if (antinode.isOnTheMap()) {
+                        antinodes.add(antinode)
+                    }
                 }
             }
         }
 
-        return antinodes.size
+        return antinodes
     }
 
     private fun Coordinates.isOnTheMap() = col in colIndices && row in rowIndices
+
+    fun printGrid(antinodes: Set<Coordinates> = setOf()) {
+        grid.forEachIndexed { r, row ->
+            row.forEachIndexed { c, char ->
+                if (antinodes.contains(Coordinates(r, c))) {
+                    print("#")
+                } else {
+                    print(char)
+                }
+            }
+            println("")
+        }
+    }
 }
