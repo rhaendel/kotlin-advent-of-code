85753887e0ac0d4d63b0c815629d8a813ca38f1b
rhaendel <rhaendel@users.noreply.github.com>
2025-01-03 14:32:35 +0100

    Restructure the project to be able to write tests
    Now, the source sets are up to Gradle's conventional project structure.
    This makes it easier to create tests in the next step.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,24 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    java
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
-    }
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.assertj:assertj-core:3.27.1")
 }
 
 tasks {
     wrapper {
         gradleVersion = "8.11.1"
     }
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    named<Test>("test") {
+        useJUnitPlatform()
+    }
 }
