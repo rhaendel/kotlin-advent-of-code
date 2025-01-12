b81e9b26d4ef3df84c59a18e38216acb24818850
rhaendel <rhaendel@users.noreply.github.com>
2025-01-12 17:45:47 +0100

    Solution Day 13, part two

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -41,20 +41,22 @@ fun main() {
         }
     }
 
-    fun solveEquations(clawMachine: ClawMachine): Int {
+    fun solveEquations(clawMachine: ClawMachine, offset: Long = 0): Long {
         with(clawMachine) {
             // formulas for a and b are deduced from the linear equations that
             // result directly from the problem statement for each slot machine
-            val a = (bx * y - by * x).toDouble() / (bx * ay - by * ax)
-            val b = (x - ax * a) / bx
+            val a = (bx * (y + offset) - by * (x + offset)).toDouble() / (bx * ay - by * ax)
+            val b = (x + offset - ax * a) / bx
             if (a.isIntegral() && b.isIntegral()) {
-                check(a <= 100 && b <= 100) {
-                    "More than 100 button presses for $clawMachine: A: $a, B: $b"
+                if (offset == 0L) {
+                    check(a <= 100 && b <= 100) {
+                        "More than 100 button presses for $clawMachine: A: $a, B: $b"
+                    }
                 }
                 check(a >= 0 && b >= 0) {
                     "Negative button presses for $clawMachine: A: $a, B: $b"
                 }
-                return (a * buttonATokens + b * buttonBTokens).toInt()
+                return (a * buttonATokens + b * buttonBTokens).toLong()
             }
             return 0
         }
@@ -88,6 +90,37 @@ fun main() {
 
     val input = readInput(day)
     printAndCheck(input, ::part1, 33427)
+
+
+    println("$day part 2")
+
+    fun part2(input: List<String>) = input
+        .parseClawMachines()
+        .map { solveEquations(it, 10000000000000) }
+        .sumOf { it }
+
+    printAndCheck(
+        """
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+            
+            Button A: X+26, Y+66
+            Button B: X+67, Y+21
+            Prize: X=12748, Y=12176
+            
+            Button A: X+17, Y+86
+            Button B: X+84, Y+37
+            Prize: X=7870, Y=6450
+            
+            Button A: X+69, Y+23
+            Button B: X+27, Y+71
+            Prize: X=18641, Y=10279
+        """.trimIndent().lines(),
+        ::part2, 875318608908
+    )
+
+    printAndCheck(input, ::part2, 91649162972270)
 }
 
 data class ClawMachine(val ax: Int, val ay: Int, val bx: Int, val by: Int, val x: Int, val y: Int)
