521bc243363b2e4e2541f9785d49af4212fcb560
rhaendel <rhaendel@users.noreply.github.com>
2025-02-09 00:50:27 +0100

    Solution Day 17, part two (Chronospatial Computer)
    * be able to save and restore the internal state
    * make the register Ints Long
    * replace the A/2^x calculations with A shr x
    * implement part two's program explicitly
      Instead of interpreting the input, run the program represented
      by day 17's input directly as Kotlin (Java byte)code.

--- src/test/kotlin/de/ronny_h/aoc/year24/day17/ChronospatialComputerTest.kt
+++ src/test/kotlin/de/ronny_h/aoc/year24/day17/ChronospatialComputerTest.kt
@@ -5,7 +5,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ChronospatialComputerTest : StringSpec({
-    val input = """
+    val input1 = """
         Register A: 729
         Register B: 0
         Register C: 0
@@ -14,6 +14,34 @@ class ChronospatialComputerTest : StringSpec({
     """.asList()
 
     "part 1: The program's output" {
-        ChronospatialComputer().part1(input) shouldBe "4,6,3,5,6,3,5,2,1,0"
+        ChronospatialComputer().part1(input1) shouldBe "4,6,3,5,6,3,5,2,1,0"
+    }
+
+    val input2 = """
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+
+        Program: 0,3,5,4,3,0
+    """.asList()
+
+    "part 2: The value of A so that the program outputs itself can be found brute force for a small program" {
+        ChronospatialComputer().part2BruteForce(input2) shouldBe "117440"
+    }
+
+    "part 2: For a larger program a different approach is needed" {
+        val program = listOf(2,4,1,1,7,5,1,5,4,0,5,5,0,3,3,0)
+        val registerABinary = findSmallestRegisterAValueToOutputTheProgram(program)
+        val registerA = registerABinary.toLong(2)
+
+        val input = """
+            Register A: $registerA
+            Register B: 0
+            Register C: 0
+    
+            Program: ${program.joinToString(",")}
+        """.asList()
+
+        ThreeBitComputer(input).runProgram() shouldBe program
     }
 })
