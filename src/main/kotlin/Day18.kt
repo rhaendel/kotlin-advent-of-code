f6081a9ac888a5d890c7bff9bddbc3e631d60c63
rhaendel <rhaendel@users.noreply.github.com>
2025-05-29 21:39:44 +0200

    Solution Day18, part two

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -7,11 +7,13 @@ import de.ronny_h.extensions.aStar
 fun main() {
     val day = "Day18"
 
+    fun List<String>.toCoordinates(): List<Coordinates> = map {
+        val (x, y) = it.split(',')
+        Coordinates(x.toInt(), y.toInt())
+    }
+
     fun part1(input: List<String>, width: Int): Int {
-        val memorySpace = MemorySpace(width, input.map {
-            val (x, y) = it.split(',')
-            Coordinates(x.toInt(), y.toInt())
-        })
+        val memorySpace = MemorySpace(width, input.toCoordinates())
         memorySpace.printGrid()
         println("-----------------")
         val shortestPath = memorySpace.shortestPath(Coordinates(0,0), Coordinates(width-1, width-1))
@@ -22,9 +24,20 @@ fun main() {
     fun part1Small(input: List<String>) = part1(input, 7) // 0..6
     fun part1Big(input: List<String>) = part1(input, 71)  // 0..70
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>, width: Int, startIndex: Int): String {
+        for (n in startIndex..input.lastIndex) {
+            val memorySpace = MemorySpace(width, input.subList(0, n).toCoordinates())
+            try {
+                memorySpace.shortestPath(Coordinates(0, 0), Coordinates(width - 1, width - 1))
+            } catch (e: IllegalStateException) {
+                return input[n-1]
+            }
+        }
+        return ""
     }
+
+    fun part2Small(input: List<String>) = part2(input, 7, 12) // 0..6
+    fun part2Big(input: List<String>) = part2(input, 71, 1024) // 0..70
 
     println("$day part 1")
 
@@ -37,13 +50,8 @@ fun main() {
 
     println("$day part 2")
 
-    printAndCheck(
-        listOf(
-            "1   6"
-        ), ::part2, 1
-    )
-    printAndCheck(testInput, ::part2, 31)
-    printAndCheck(input, ::part2, 18805872)
+    printAndCheck(testInput, ::part2Small, "6,1")
+    printAndCheck(input, ::part2Big, "50,23")
 }
 
 private class MemorySpace(width: Int, corrupted: List<Coordinates>) : Grid<Char>(width, width, '.', '#', corrupted) {
