da76ce356dccdbeb324c4f506b71d88719a2e107
rhaendel <rhaendel@users.noreply.github.com>
2025-06-08 23:34:22 +0200

    Rename Utils to Hashing
    This name suits much better since the other functions have moved out
    already and the file's single responsibility has become clear.

--- src/test/kotlin/de/ronny_h/aoc/extensions/HashingTest.kt
+++ src/test/kotlin/de/ronny_h/aoc/extensions/HashingTest.kt
@@ -3,7 +3,7 @@ package de.ronny_h.aoc.extensions
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class UtilsTest : StringSpec({
+class HashingTest : StringSpec({
 
     "md5() converts a String to an MD5 hash" {
         "This is just a test".md5() shouldBe "df0a9498a65ca6e20dc58022267f339a"
