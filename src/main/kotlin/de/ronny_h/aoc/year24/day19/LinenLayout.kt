9188dbeadb39a00cb8ca2b7c4d77632a249f4eef
rhaendel <rhaendel@users.noreply.github.com>
2025-06-08 13:29:42 +0200

    Make Day19's implementation a class, express the small examples as tests
    Coverage:
    - class: 77%
    - method: 79%
    - line: 80%
    - branch: 83%

---
+++ src/main/kotlin/de/ronny_h/aoc/year24/day19/LinenLayout.kt
@@ -0,0 +1,41 @@
+package de.ronny_h.aoc.year24.day19
+
+import de.ronny_h.aoc.extensions.PrefixTree
+import printAndCheck
+import readInput
+
+fun main() {
+    val day = "Day19"
+    val input = readInput(day)
+    val linenLayout = LinenLayout()
+
+    println("$day part 1")
+    printAndCheck(input, linenLayout::part1, 251)
+
+    println("$day part 2")
+    printAndCheck(input, linenLayout::part2, 616957151871345)
+}
+
+class LinenLayout {
+    fun part1(input: List<String>): Int {
+        val towels = input.parseTowels()
+        val designs = input.parseDesigns()
+        println("${towels.size} towels, ${designs.size} designs")
+
+        return designs.count { it.isPossibleWith(towels) }
+    }
+
+    fun part2(input: List<String>): Long {
+        val towels = input.parseTowels()
+        val designs = input.parseDesigns()
+        println("${towels.size} towels, ${designs.size} designs")
+
+        return designs.sumOf { it.countPossibilitiesWith(towels) }
+    }
+
+    private fun List<String>.parseTowels(): List<String> = this.first().split(", ")
+    private fun List<String>.parseDesigns(): List<String> = drop(2)
+
+    private fun String.isPossibleWith(towels: List<String>): Boolean = (this.countPossibilitiesWith(towels) > 0)
+    private fun String.countPossibilitiesWith(towels: List<String>): Long = PrefixTree().insert(this, towels)
+}
