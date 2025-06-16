77dfee977415e73ac30daf775abf79f9ec5cdf71
rhaendel <rhaendel@users.noreply.github.com>
2025-06-16 23:48:42 +0200

    Solution Day 24, part one (Crossed Wires)

---
+++ src/main/kotlin/de/ronny_h/aoc/year24/day24/CrossedWires.kt
@@ -0,0 +1,77 @@
+package de.ronny_h.aoc.year24.day24
+
+import de.ronny_h.aoc.AdventOfCode
+
+fun main() = CrossedWires().run(66055249060558, 0)
+
+class CrossedWires: AdventOfCode<Long>(2024, 24) {
+
+    companion object {
+        val and = { a: Boolean, b: Boolean -> a && b }
+        val or = { a: Boolean, b: Boolean -> a || b }
+        val xor = { a: Boolean, b: Boolean -> a xor b }
+    }
+
+    fun parseWires(input: List<String>) = input
+        .takeWhile { it.isNotEmpty() }
+        .map {
+            val (name, value) = it.split(": ")
+            Wire(name, value == "1")
+        }
+
+    fun parseGates(input: List<String>) = input
+        .dropWhile { it.isNotEmpty() }
+        .drop(1)
+        .map {
+            val (term, result) = it.split(" -> ")
+            val (in1, op, in2) = term.split(" ")
+            val operation = when(op) {
+                "AND" -> and
+                "OR" -> or
+                "XOR" -> xor
+                else -> error("Operation not supported: $op")
+            }
+            Gate(in1.trim(), in2.trim(), operation, result.trim())
+        }
+
+    override fun part1(input: List<String>): Long {
+        val wires = parseWires(input)
+            .associateBy(Wire::name)
+            .toMutableMap()
+        val gates = parseGates(input)
+        val zWiresToCalculate = gates.count { it.out.startsWith("z") }
+
+        do {
+            val simulatableGates = gates.filter { it.in1 in wires && it.in2 in wires }
+            val outWires = simulatableGates
+                .map { it.simulateWith(wires) }
+                .associateBy(Wire::name)
+            wires.putAll(outWires)
+        } while (zWiresToCalculate != wires.count { it.key.startsWith("z") })
+
+        return wires.values
+            .filter { it.name.startsWith("z") }
+            .sortedByDescending(Wire::name)
+            .joinToString("") {
+                when (it.value) {
+                    true -> "1"
+                    false -> "0"
+                }
+            }
+            .toLong(2)
+    }
+
+    override fun part2(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+}
+
+data class Wire(val name: String, val value: Boolean)
+data class Gate(val in1: String, val in2: String, val operation: (Boolean, Boolean) -> (Boolean), val out: String) {
+    fun simulateWith(inWires: Map<String, Wire>): Wire = Wire(
+        out,
+        operation(inWires.valueOf(in1), inWires.valueOf(in2))
+    )
+}
+
+private fun Map<String, Wire>.valueOf(inWire: String): Boolean = getValue(inWire).value
