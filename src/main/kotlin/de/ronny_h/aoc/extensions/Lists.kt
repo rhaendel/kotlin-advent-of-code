f5712541a502fdb9b4be316e6d70482149d00658
rhaendel <rhaendel@users.noreply.github.com>
2025-06-21 14:56:32 +0200

    A split function for List<String>

---
+++ src/main/kotlin/de/ronny_h/aoc/extensions/Lists.kt
@@ -0,0 +1,28 @@
+package de.ronny_h.aoc.extensions
+
+/**
+ * Splits the list into multiple sub-lists on lines that match the specified delimiter.
+ * @param delimiter The line to split the list at. Lines from the original list exactly matching the [delimiter] are not part
+ * of any of the resulting sub-lists.
+ * @return A list containing all sub-lists of the original list splitted at lines matching the [delimiter].
+ *
+ * * Consecutive lines matching the [delimiter] do not produce empty sub-lists in the result.
+ * * If the original list ends with a [delimiter], the returned list does not contain the last empty sub-list.
+ */
+fun List<String>.split(delimiter: String = ""): List<List<String>> {
+    if (isEmpty()) return emptyList()
+    val result = fold(mutableListOf<MutableList<String>>(mutableListOf())) { acc, line ->
+        if (line == delimiter) {
+            if (acc.last().isNotEmpty()) {
+                acc.add(mutableListOf())
+            }
+        } else {
+            acc.last().add(line)
+        }
+        acc
+    }
+    if (this.last() == delimiter) {
+        result.removeAt(result.lastIndex)
+    }
+    return result
+}
---
+++ src/test/kotlin/de/ronny_h/aoc/extensions/ListsTest.kt
@@ -0,0 +1,34 @@
+package de.ronny_h.aoc.extensions
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class ListsTest : StringSpec({
+    "split an empty list" {
+        listOf<String>().split("") shouldBe listOf()
+    }
+
+    "split a list with only a single block" {
+        listOf("a", "b").split("") shouldBe listOf(listOf("a", "b"))
+    }
+
+    "split a list with multiple blocks" {
+        listOf("a", "b", "", "c", "d").split("") shouldBe listOf(listOf("a", "b"), listOf("c", "d"))
+    }
+
+    "split a list with multiple blocks and consecutive delimiting lines" {
+        listOf("a", "b", "", "", "", "c", "d").split("") shouldBe listOf(listOf("a", "b"), listOf("c", "d"))
+    }
+
+    "split a list with multiple blocks using the default delimiter" {
+        listOf("a", "b", "", "c", "d").split() shouldBe listOf(listOf("a", "b"), listOf("c", "d"))
+    }
+
+    "split a list with multiple blocks using a non-empty delimiter" {
+        listOf("a", "b", "---", "c", "d").split("---") shouldBe listOf(listOf("a", "b"), listOf("c", "d"))
+    }
+
+    "split a list with multiple blocks ending with the delimiter line" {
+        listOf("a", "b", "", "c", "d", "").split("") shouldBe listOf(listOf("a", "b"), listOf("c", "d"))
+    }
+})
