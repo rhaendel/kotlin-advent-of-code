3b8ac1015d7fe0c8fe445d7f6ae5cbc3904c2e99
rhaendel <rhaendel@users.noreply.github.com>
2025-06-25 21:46:28 +0200

    Print an error message when the expected input file is missing

--- src/main/kotlin/de/ronny_h/aoc/AdventOfCode.kt
+++ src/main/kotlin/de/ronny_h/aoc/AdventOfCode.kt
@@ -1,7 +1,5 @@
 package de.ronny_h.aoc
 
-import kotlin.io.path.Path
-import kotlin.io.path.readText
 import kotlin.time.TimedValue
 import kotlin.time.measureTimedValue
 
@@ -13,11 +11,16 @@ abstract class AdventOfCode<T>(val year: Int, val day: Int) {
         val input = readInput()
         println("Day $day $year - ${this::class.simpleName} - part 1")
         printAndCheck(input, ::part1, expectedPart1)
+        println()
         println("Day $day $year - ${this::class.simpleName} - part 2")
         printAndCheck(input, ::part2, expectedPart2)
     }
 
-    fun readInput() = AdventOfCode::class.java.getResource("/$year/Day${paddedDay()}.txt")!!.readText().trim().lines()
+    fun readInput(): List<String> {
+        val inputFile = "/$year/Day${paddedDay()}.txt"
+        return AdventOfCode::class.java.getResource(inputFile)?.readText()?.trim()?.lines()
+            ?: error("The input file for day $day, year $year could not be found. Expected at '$inputFile' in the resources folder.")
+    }
 
     private fun paddedDay(): String = day.toString().padStart(2, '0')
 
