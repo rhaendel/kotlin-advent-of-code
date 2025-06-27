a55043815640fa11c7bef3acdc6c2a1eb772f79b
rhaendel <rhaendel@users.noreply.github.com>
2025-06-27 19:01:55 +0200

    Gradle build cleanup
    * Remove redundant java plugin (comes transitively with kotlin jvm)
    * assertj is not used anymore (kotest comes with its own assertions)
    * logback-classic only needs to be a runtime dependency
    * The gradle wrapper task configuration used an outdated version.
      Since dependabot updates the wrapper, we don't need that config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "2.2.0"
-    java
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
@@ -11,12 +10,10 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.0.M4")
-    testImplementation("org.assertj:assertj-core:3.27.3")
-    testImplementation("ch.qos.logback:logback-classic:1.5.18")
-
     // for tapSystemOut
     testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.5.18")
 }
 
 java {
@@ -26,9 +23,6 @@ java {
 }
 
 tasks {
-    wrapper {
-        gradleVersion = "8.11.1"
-    }
     named<Test>("test") {
         useJUnitPlatform()
     }
