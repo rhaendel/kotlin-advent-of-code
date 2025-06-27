5cde78d544d9f8b97cc4f0d1be412f88277d8f01
rhaendel <rhaendel@users.noreply.github.com>
2025-06-27 14:41:34 +0200

    Refactor PerfectlySphericalHouses to be less repeating

--- src/main/kotlin/de/ronny_h/aoc/year2015/day03/PerfectlySphericalHouses.kt
+++ src/main/kotlin/de/ronny_h/aoc/year2015/day03/PerfectlySphericalHouses.kt
@@ -7,44 +7,45 @@ import de.ronny_h.aoc.extensions.Direction
 fun main() = PerfectlySphericalHouses().run(2592, 2360)
 
 class PerfectlySphericalHouses : AdventOfCode<Int>(2015, 3) {
-    override fun part1(input: List<String>): Int {
-        require(input.size == 1)
-        var position = Coordinates(0, 0)
-        val visited = mutableSetOf<Coordinates>(position)
-        input.parseDirections()
-            .forEach {
-                position += it
-                visited.add(position)
-            }
-        return visited.size
-    }
+    override fun part1(input: List<String>): Int = input
+        .parseDirections()
+        .visitCoordinates()
+        .size
 
     override fun part2(input: List<String>): Int {
-        require(input.size == 1)
-        var santaPosition = Coordinates(0, 0)
-        var robotSantaPosition = santaPosition
-        val visited = mutableSetOf<Coordinates>(santaPosition)
-        input.parseDirections()
-            .forEachIndexed { i, direction ->
-                if (i % 2 == 0) {
-                    robotSantaPosition += direction
-                    visited.add(robotSantaPosition)
-                } else {
-                    santaPosition += direction
-                    visited.add(santaPosition)
-                }
-            }
-        return visited.size
+        val (santa, roboSanta) = input
+            .parseDirections()
+            .withIndex()
+            .partition { it.index % 2 == 0 }
+
+        return santa.map { it.value }.visitCoordinates()
+            .union(
+                roboSanta.map { it.value }.visitCoordinates()
+            )
+            .size
     }
 
-    private fun List<String>.parseDirections() = first()
-        .map {
-            when (it) {
-                '^' -> Direction.NORTH
-                'v' -> Direction.SOUTH
-                '>' -> Direction.EAST
-                '<' -> Direction.WEST
-                else -> error("invalid direction: $it")
+    private fun List<String>.parseDirections(): List<Direction> {
+        require(size == 1)
+        return first()
+            .map {
+                when (it) {
+                    '^' -> Direction.NORTH
+                    'v' -> Direction.SOUTH
+                    '>' -> Direction.EAST
+                    '<' -> Direction.WEST
+                    else -> error("invalid direction: $it")
+                }
             }
+    }
+
+    private fun List<Direction>.visitCoordinates(): Set<Coordinates> {
+        var position = Coordinates(0, 0)
+        val visited = mutableSetOf<Coordinates>(position)
+        forEach {
+            position += it
+            visited.add(position)
         }
+        return visited
+    }
 }
