ded26882adcf5a28c88eba1b4ab01cf389602538
rhaendel <rhaendel@users.noreply.github.com>
2025-06-28 21:46:56 +0200

    Solution 2015-06 (Probably a Fire Hazard)

---
+++ src/main/kotlin/de/ronny_h/aoc/year2015/day06/FireHazard.kt
@@ -0,0 +1,73 @@
+package de.ronny_h.aoc.year2015.day06
+
+import de.ronny_h.aoc.AdventOfCode
+import de.ronny_h.aoc.extensions.Coordinates
+import de.ronny_h.aoc.year2015.day06.Action.OFF
+import de.ronny_h.aoc.year2015.day06.Action.ON
+import de.ronny_h.aoc.year2015.day06.Action.TOGGLE
+
+fun main() = FireHazard().run(400410, 15343601)
+
+class FireHazard : AdventOfCode<Int>(2015, 6) {
+
+    private val onOfGrid = List(1000) {
+        MutableList(1000) { false }
+    }
+
+    private fun switch(switchLight: SwitchLight) = with(switchLight) {
+        for (row in from.row..to.row) {
+            for (col in from.col..to.col) {
+                when(action) {
+                    ON -> onOfGrid[row][col] = true
+                    OFF -> onOfGrid[row][col] = false
+                    TOGGLE -> onOfGrid[row][col] = !onOfGrid[row][col]
+                }
+            }
+        }
+    }
+
+    override fun part1(input: List<String>): Int {
+        input.map(::parse).forEach(::switch)
+        return onOfGrid.sumOf { row -> row.count { it } }
+    }
+
+    fun parse(line: String): SwitchLight {
+        val action = when {
+            line.startsWith(ON.text) -> ON
+            line.startsWith(OFF.text) -> OFF
+            line.startsWith(TOGGLE.text) -> TOGGLE
+            else -> error("invalid start of line: $line")
+        }
+        val (from, to) = line.substring(action.text.length).split(" through ")
+        val (fromRow, fromCol) = from.split(',').map(String::toInt)
+        val (toRow, toCol) = to.split(',').map(String::toInt)
+        return SwitchLight(action, Coordinates(fromRow, fromCol), Coordinates(toRow, toCol))
+    }
+
+    private val dimmingGrid = List(1000) {
+        MutableList(1000) { 0 }
+    }
+
+    private fun dim(switchLight: SwitchLight) = with(switchLight) {
+        for (row in from.row..to.row) {
+            for (col in from.col..to.col) {
+                when(action) {
+                    ON -> dimmingGrid[row][col]++
+                    OFF -> if (dimmingGrid[row][col] > 0) dimmingGrid[row][col]--
+                    TOGGLE -> dimmingGrid[row][col] = dimmingGrid[row][col] + 2
+                }
+            }
+        }
+    }
+
+    override fun part2(input: List<String>): Int {
+        input.map(::parse).forEach(::dim)
+        return dimmingGrid.sumOf { row -> row.sumOf { it } }
+    }
+}
+
+data class SwitchLight(val action: Action, val from: Coordinates, val to: Coordinates)
+
+enum class Action(val text: String) {
+    ON("turn on "), OFF("turn off "), TOGGLE("toggle ")
+}
---
+++ src/test/kotlin/de/ronny_h/aoc/year2015/day06/FireHazardTest.kt
@@ -0,0 +1,29 @@
+package de.ronny_h.aoc.year2015.day06
+
+import de.ronny_h.aoc.extensions.Coordinates
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class FireHazardTest : StringSpec({
+
+    "turn can be parsed" {
+        FireHazard().parse("turn on 1,2 through 3,4") shouldBe SwitchLight(Action.ON, Coordinates(1,2), Coordinates(3,4))
+        FireHazard().parse("turn off 10,20 through 30,40") shouldBe SwitchLight(Action.OFF, Coordinates(10,20), Coordinates(30,40))
+        FireHazard().parse("toggle 0,0 through 999,999") shouldBe SwitchLight(Action.TOGGLE, Coordinates(0,0), Coordinates(999,999))
+    }
+
+    "part 1: turn on every light" {
+        FireHazard().part1(listOf("turn on 0,0 through 999,999")) shouldBe 1_000_000
+    }
+
+    "part 1: toggle the first line" {
+        FireHazard().part1(listOf("toggle 0,0 through 999,0")) shouldBe 1000
+    }
+
+    "part 2: a brightness of one" {
+        FireHazard().part2(listOf("turn on 0,0 through 0,0")) shouldBe 1
+    }
+    "part 2: a brightness of 2000000" {
+        FireHazard().part2(listOf("toggle 0,0 through 999,999")) shouldBe 2_000_000
+    }
+})
