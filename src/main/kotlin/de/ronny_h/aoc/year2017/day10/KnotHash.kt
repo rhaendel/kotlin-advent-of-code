0c38527f09b20d8ee50498c537806c1645287164
rhaendel <rhaendel@users.noreply.github.com>
2025-08-16 17:12:05 +0200

    Solution 2017-10 (Knot Hash)

---
+++ src/main/kotlin/de/ronny_h/aoc/year2017/day10/KnotHash.kt
@@ -0,0 +1,62 @@
+package de.ronny_h.aoc.year2017.day10
+
+import de.ronny_h.aoc.AdventOfCode
+
+fun main() = KnotHash().run("3770", "a9d0e68649d0174c8756a59ba21d4dc6")
+
+class KnotHash : AdventOfCode<String>(2017, 10) {
+    override fun part1(input: List<String>): String {
+        val lengths = input.single().split(",").map(String::toInt)
+        val size = if (lengths.size < 5) 5 else 256  // just for testing
+        return knotHash(size, lengths).toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        val lengths = input.single().toASCII() + listOf(17, 31, 73, 47, 23)
+        val denseHash = sparseHash(256, lengths, 64).reduceToDenseHash()
+        check(denseHash.size == 16)
+        return denseHash.joinToString("") { it.toString(16).padStart(2, '0') }
+    }
+}
+
+fun knotHash(size: Int, lengths: List<Int>): Int {
+    val circularList = sparseHash(size, lengths)
+    return circularList[0] * circularList[1]
+}
+
+private fun sparseHash(
+    size: Int,
+    lengths: List<Int>,
+    rounds: Int = 1,
+): List<Int> {
+    val circularList = MutableList(size) { it }
+    var position = 0
+    var skipSize = 0
+
+    fun reverseSubList(start: Int, length: Int) {
+        if (start + length < size) {
+            circularList.subList(start, start + length).toList()
+        } else {
+            circularList.subList(start, size) + circularList.subList(0, length - size + start)
+        }
+            .reversed()
+            .forEachIndexed { i, item ->
+                circularList[(start + i) % size] = item
+            }
+    }
+
+    repeat(rounds) {
+        lengths.forEach { length ->
+            reverseSubList(position, length)
+            position = (position + length + skipSize) % size
+            skipSize++
+        }
+    }
+    return circularList
+}
+
+fun List<Int>.reduceToDenseHash() = chunked(16) {
+    it.reduce { a, b -> a xor b }
+}
+
+fun String.toASCII() = map(Char::code)
---
+++ src/test/kotlin/de/ronny_h/aoc/year2017/day10/KnotHashTest.kt
@@ -0,0 +1,37 @@
+package de.ronny_h.aoc.year2017.day10
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class KnotHashTest : StringSpec({
+
+    "the knot hash of a small example" {
+        knotHash(5, listOf(3, 4, 1, 5)) shouldBe 12
+    }
+
+    "part 1: the knot hash of a small example" {
+        val input = listOf("3,4,1,5")
+        KnotHash().part1(input) shouldBe "12"
+    }
+
+    "ASCII Codes of the input" {
+        "1,2,3".toASCII() shouldBe listOf(49, 44, 50, 44, 51)
+    }
+
+    "reduce to dense hash" {
+        listOf(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22).reduceToDenseHash() shouldBe listOf(64)
+    }
+
+    "part 2: the real knot hash" {
+        forAll(
+            row(listOf(""), "a2582a3a0e66e6e86e3812dcb672a272"),
+            row(listOf("AoC 2017"), "33efeb34ea91902bb2f59c9920caa6cd"),
+            row(listOf("1,2,3"), "3efbe78a8d82f29979031a4aa0b16a9d"),
+            row(listOf("1,2,4"), "63960835bcdc130f0b66d7ff4f6a5a8e"),
+        ) { input, hash ->
+            KnotHash().part2(input) shouldBe hash
+        }
+    }
+})
