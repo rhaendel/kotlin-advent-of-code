1708f473eea9b897712cd30a421599d8868b4376
rhaendel <rhaendel@users.noreply.github.com>
2025-08-19 15:10:52 +0200

    Add missing dependency kotest-assertions-table
    It was moved out of kotest-assertions-core with version 6.0.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // for tapSystemOut
     testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
     testImplementation("io.kotest:kotest-runner-junit5:6.0.0")
+    testImplementation("io.kotest:kotest-assertions-table:6.0.0")
     testRuntimeOnly("ch.qos.logback:logback-classic:1.5.18")
 }
 
