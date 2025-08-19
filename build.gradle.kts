a341a131ce67b476a8d51f9e104eacbe437e6155
rhaendel <rhaendel@users.noreply.github.com>
2025-08-19 15:36:36 +0200

    Introduce a Gradle version catalog
    Move all dependency versions to libs.versions.toml.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.10"
-    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.kover)
 }
 
 repositories {
@@ -8,13 +8,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation(libs.kotlinx.coroutines.core)
 
     // for tapSystemOut
-    testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.0")
-    testImplementation("io.kotest:kotest-assertions-table:6.0.0")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.5.18")
+    testImplementation(libs.system.lambda)
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.kotest.assertions.table)
+    testRuntimeOnly(libs.logback.classic)
 }
 
 java {
