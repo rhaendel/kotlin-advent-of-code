2b65f91be69a9c96b8930a545e04ff988c7da05f
rhaendel <rhaendel@users.noreply.github.com>
2025-12-31 13:39:16 +0100

    2018-19, part 2, third step (Go With The Flow)
    Inline and simplify the register variables.

--- src/main/kotlin/de/ronny_h/aoc/year2018/day19/GoWithTheFlow.kt
+++ src/main/kotlin/de/ronny_h/aoc/year2018/day19/GoWithTheFlow.kt
@@ -4,7 +4,6 @@ import de.ronny_h.aoc.AdventOfCode
 import de.ronny_h.aoc.year2018.day16.ProgramStep
 import de.ronny_h.aoc.year2018.day16.WristDevice
 
-// part 2: wrong: 0
 fun main() = GoWithTheFlow().run(1694, 0)
 
 class GoWithTheFlow : AdventOfCode<Int>(2018, 19) {
@@ -48,61 +47,38 @@ class WristDeviceWithFlowControl(input: List<String>, registerZeroInitValue: Int
 }
 
 class HardCodedWristDevice {
-    private var r0 = 0
-    private var r1 = 0
-    private var r2 = 0
-    private var r3 = 0
-    private var r5 = 0
 
-    fun one() {
-        r3 = 1
+    fun one(r5: Int): Int {
+        var result = 0
+        var r3 = 1
         do {
-            r1 = 1
+            var r1 = 1
             do {
-                r2 = r3 * r1
-                if (r2 == r5) {
-                    r2 = 1
-                    r0 += r3
-                } else {
-                    r2 = 0
+                if (r3 * r1 == r5) {
+                    result += r3
                 }
-                r1 += 1
-                r2 = if (r1 > r5) 1 else 0
+                r1++
             } while (r1 <= r5)
-            r3 += 1
-            r2 = if (r3 > r5) 1 else 0
+            r3++
         } while (r3 <= r5)
+        return result
     }
 
-    fun partOne(): Int {
-        r5 += 2
-        r5 *= r5
-        r5 *= 19
-        r5 *= 11
-        r2 += 5
-        r2 *= 22
-        r2 += 18
-        r5 += r2
+    fun partOne(isPartOne: Boolean = true): Int {
+        var r5 = 2 * 2 * 19 * 11 + (5 * 22 + 18) // 964
+        println("r5: $r5")
 
-        if (r0 == 0) {
-            one()
-            return r0
+        if (isPartOne) {
+            return one(r5)
         }
 
-        r2 = 27
-        r2 *= 28
-        r2 += 29
-        r2 *= 30
-        r2 *= 14
-        r2 *= 32
-        r5 += r2
-        r0 = 0
-        one()
-        return r0
+        val toAdd = (27 * 28 + 29) * 30 * 14 * 32 // 10550400
+        println("toAdd: $toAdd")
+        r5 += toAdd
+        return one(r5)
     }
 
     fun partTwo(): Int {
-        r0 = 1
-        return partOne()
+        return partOne(false)
     }
 }
