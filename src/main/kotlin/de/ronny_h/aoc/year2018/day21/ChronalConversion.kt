54ec28e73ad4d2159c76095ac9e448d405c17c67
rhaendel <rhaendel@users.noreply.github.com>
2026-06-14 13:12:20 +0200

    2018-21 (Chronal Conversion): Simplify the ActivationSystem
    By inlining variables and moving if conditions into while
    conditions.

--- src/main/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversion.kt
+++ src/main/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversion.kt
@@ -14,9 +14,10 @@ class ChronalConversion : AdventOfCode<Int>(2018, 21) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun part1(input: List<String>): Int {
-        runTheActualProgram(input)
-        // ActivationSystem().run()
-        return 0
+        // runTheActualProgram(input)
+        val registerZero = 0
+        ActivationSystem(registerZero).run()
+        return registerZero
     }
 
     private fun runTheActualProgram(input: List<String>) = runBlocking {
@@ -51,61 +52,36 @@ class ChronalConversion : AdventOfCode<Int>(2018, 21) {
     }
 }
 
-class ActivationSystem {
-    var reg0 = 0
-    var reg1 = 0
-    var reg2 = 0
-    var reg3 = 0
-    var reg4 = 0
+class ActivationSystem(private val reg0: Int = 0) {
+    private var reg2 = 0
+    private var reg3 = 0
+    private var reg4 = 0
 
     fun run() {
         reg4 = 0
 
-        while (true) {
+        do {
             reg3 = reg4 or 65536
             reg4 = 4332021
 
             while (true) {
                 reg2 = reg3 and 255
-                reg4 = reg4 + reg2
+                reg4 += reg2
                 reg4 = reg4 and 16777215
-                reg4 = reg4 * 65899
+                reg4 *= 65899
                 reg4 = reg4 and 16777215
 
-                if (256 > reg3) {
-                    reg2 = 1
-                } else {
-                    reg2 = 0
-                }
-
-                if (256 > reg3) {
+                if (reg3 < 256) {
                     break
                 }
 
                 reg2 = 0
-
-                while (true) {
-                    reg1 = reg2 + 1
-                    reg1 = reg1 * 256
-                    if (reg1 > reg3) {
-                        reg1 = 1
-                        break
-                    } else {
-                        reg1 = 0
-                    }
-
-                    reg2 = reg2 + 1
+                while ((reg2 + 1) * 256 <= reg3) {
+                    reg2++
                 }
 
                 reg3 = reg2
             }
-
-            if (reg4 == reg0) {
-                reg2 = 1
-                return
-            } else {
-                reg2 = 0
-            }
-        }
+        } while (reg4 != reg0)
     }
 }
