b7538bbd3eb30c620287d34b1d3645c5cb193b12
rhaendel <rhaendel@users.noreply.github.com>
2026-06-14 14:30:29 +0200

    Solution 2018-21, part 2 (Chronal Conversion)

--- src/main/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversion.kt
+++ src/main/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversion.kt
@@ -2,7 +2,7 @@ package de.ronny_h.aoc.year2018.day21
 
 import de.ronny_h.aoc.AdventOfCode
 
-fun main() = ChronalConversion().run(9566170, 0)
+fun main() = ChronalConversion().run(9566170, 13192622)
 
 class ChronalConversion : AdventOfCode<Int>(2018, 21) {
 
@@ -11,7 +11,7 @@ class ChronalConversion : AdventOfCode<Int>(2018, 21) {
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        return ActivationSystem().run(false)
     }
 }
 
@@ -20,23 +20,38 @@ class ActivationSystem {
     private var reg3 = 0
     private var reg4 = 0
 
-    fun run(): Int {
-        reg3 = 65536
-        reg4 = 4332021
+    fun run(returnFirst: Boolean = true): Int {
+        reg4 = 0
 
-        while (true) {
-            reg2 = reg3 and 255
-            reg4 += reg2
-            reg4 = reg4 and 16777215
-            reg4 *= 65899
-            reg4 = reg4 and 16777215
+        val set = mutableSetOf<Int>()
+        var last = 0
+        do {
+            reg3 = reg4 or 65536
+            reg4 = 4332021
 
-            if (reg3 < 256) {
-                break
+            while (true) {
+                reg2 = reg3 and 255
+                reg4 += reg2
+                reg4 = reg4 and 16777215
+                reg4 *= 65899
+                reg4 = reg4 and 16777215
+
+                if (reg3 < 256) {
+                    break
+                }
+
+                reg3 /= 256
             }
-
-            reg3 /= 256
-        }
-        return reg4
+            if (returnFirst) {
+                return reg4
+            }
+            if (!set.add(reg4)) {
+                // the first time we see a value again, the cycle starts from the beginning
+                // -> the last value was the lowest non-negative integer value for register 0 that causes the program to
+                //    halt after executing the most instructions
+                return last
+            }
+            last = reg4
+        } while (true)
     }
 }
--- src/test/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversionTest.kt
+++ src/test/kotlin/de/ronny_h/aoc/year2018/day21/ChronalConversionTest.kt
@@ -1,26 +1,15 @@
 package de.ronny_h.aoc.year2018.day21
 
 import de.infix.testBalloon.framework.core.testSuite
-import de.ronny_h.aoc.extensions.asList
 import io.kotest.matchers.shouldBe
 
 val ChronalConversionTest by testSuite {
 
-    test("part 1: The lowest value for register 0 that halts the program") {
-        // just feed in the bitwise operations on numbers check
-        val input = """
-            #ip 5
-            seti 123 0 4
-            bani 4 456 4
-            eqri 4 72 4
-            addr 4 5 5
-        """.asList()
-
-        ChronalConversion().part1(input) shouldBe 9566170
+    test("part 1: The lowest value for register 0 that halts the program with fewest instructions") {
+        ChronalConversion().part1(emptyList()) shouldBe 9566170
     }
 
-    test("part 2") {
-        val input = listOf("")
-        ChronalConversion().part2(input) shouldBe 0
+    test("part 2: The lowest value for register 0 that halts the program with most instructions") {
+        ChronalConversion().part2(emptyList()) shouldBe 13192622
     }
 }
